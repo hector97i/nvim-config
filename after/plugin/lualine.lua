38417beefa8b415319a3babf4e07c5c8f6ef54ba
Hector Duran <hector.duran@finmancorp.com>
2024-01-17 19:05:00 -0600

    updates to lualine config

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,10 +1,24 @@
 local setup, lualine = pcall(require, "lualine")
-if not setup then return end
+-- if not setup then return end
+
+sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+},
 
 lualine.setup({
     options = {
         theme = "seoul256", -- uncomment for dark theme
         -- theme = "onelight", -- uncomment for light theme
+        icons_enabled = false,
+        global_status = true,
+        always_divide_middle = false,
+        sections = sections,
+        inactive_sections = sections,
     },
 })
 
